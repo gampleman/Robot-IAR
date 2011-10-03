2504d47f9e9c3020d7fbebbebd1bd1ed5b41a476
Jakub Hampl <honitom@seznam.cz>
2011-10-03 13:46:52 +0100

    Engaged servo

--- src/Movement.c
+++ src/Movement.c
@@ -14,12 +14,15 @@ int driveRightMotor(float Value)
 
 int turnOnSpotLeft()
 {
-	
+	turnServo(-50);
+	driveRightMotor(-40);
+	driveLeftMotor(50);
 }
 
 int turnServo(float Value)
 {
 	CPhidgetAdvancedServo_setPosition (servo, 0, Value);
+	CPhidgetAdvancedServo_setEngaged(servo, 0, 1);
 }
 
 int turnOnSpotRight()
@@ -34,5 +37,6 @@ int stop()
 {
 	driveRightMotor(0.0);
 	driveLeftMotor(0.0);
+	CPhidgetAdvancedServo_setEngaged(servo, 0, 0);
 	return 0;
 }
