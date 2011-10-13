c49c81df168ed82da439189d9b615ec3737be035
Jakub Hampl <honitom@seznam.cz>
2011-10-13 10:20:18 +0100

    Debugging

--- src/Behavior.c
+++ src/Behavior.c
@@ -1,27 +1,27 @@
 #define TURNING_DURATION 4
 #define LIGHT_THRESHOLD 160
 void behave() {
+  printf("\n\n\n\nLeft Light: %d, Right Light: %d\n\n\n\n\n", state.LeftLight, state.RightLight);
   if(state.LeftLight > LIGHT_THRESHOLD && state.RightLight < LIGHT_THRESHOLD) {
     turnOnSpotLeft();
-    stop();
-    sleep(2);
+
   }
   else if (state.LeftLight < LIGHT_THRESHOLD && state.RightLight > LIGHT_THRESHOLD) {
     turnOnSpotRight(); 
-    stop();
-    sleep(2);
+
   }
   else if (state.LeftLight > LIGHT_THRESHOLD && state.RightLight > LIGHT_THRESHOLD) {
     // orientStraightAndDrive();
     // sleep(2);
-    // stop();
+    //stop();
     // sleep(2);
     // driveBack();
     // sleep(2);
     // turnOnSpotLeft();
     // sleep(2);
-    stop();
-    sleep(2);
+    //stop();
+    //sleep(2);
+    exit(0);
     if(state.LeftWhisker && state.RightWhisker == 0)  {
       goTowards(80);
     } else if(state.RightWhisker && state.LeftWhisker == 0)  {
@@ -37,22 +37,22 @@ void behave() {
       orientStraightAndDrive();
     }
   }
-	else if(state.LeftWhisker)  {
-		retreat(0);
-		sleep(1);
-		driveBack();
-	}
-	else if(state.RightWhisker) {
-		retreat(1);
-		sleep(1); 
-		driveBack();
-	}
-	else if(state.FrontFacingIR > 350) {
-		driveBack();
-		retreat(1);
-		sleep(1);
-		driveBack();
-	}
+  // else if(state.LeftWhisker)  {
+  //  retreat(0);
+  //  sleep(1);
+  //  driveBack();
+  // }
+  // else if(state.RightWhisker) {
+  //  retreat(1);
+  //  sleep(1); 
+  //  driveBack();
+  // }
+  // else if(state.FrontFacingIR > 350) {
+  //  driveBack();
+  //  retreat(1);
+  //  sleep(1);
+  //  driveBack();
+  // }
 	else if(timer.iteration > timer.threshold && timer.iteration < timer.threshold + TURNING_DURATION) {
     turnOnSpotLeft();
 	}
