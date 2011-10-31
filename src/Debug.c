aff4c269c822a231fe31c87dcdb1dacfb175667a
Jakub Hampl <honitom@seznam.cz>
2011-10-31 14:20:45 +0000

    Insene cool logging

--- src/Debug.c
+++ src/Debug.c
@@ -1,26 +1,28 @@
 /*
 Debugging utilities. Enable debugging in one area by compling with `make DEBUG=flag` where flag is one of BEHAVIOR, MOVEMENT, SETUP or SENSOR.
 */
-FILE *behavior;
-FILE *movement;
-FILE *sensor;
-FILE *setup;
+
+FILE *logbehavior;
+FILE *logmovement;
+FILE *logsensor;
+FILE *logsetup;
+
 
 
 int init_debugging() {
-  if((behavior=fopen("behavior.log", "w")) == NULL) {
+  if((logbehavior = fopen("behavior.log", "w")) == NULL) {
     printf("Cannot open file.\n");
     return 1;
   }
-  if((movement=fopen("movement.log", "w")) == NULL) {
+  if((logmovement=fopen("movement.log", "w")) == NULL) {
     printf("Cannot open file.\n");
     return 1;
   }
-  if((sensor=fopen("sensor.log", "w")) == NULL) {
+  if((logsensor=fopen("sensor.log", "w")) == NULL) {
     printf("Cannot open file.\n");
     return 1;
   }
-  if((setup=fopen("setup.log", "w")) == NULL) {
+  if((logsetup=fopen("setup.log", "w")) == NULL) {
     printf("Cannot open file.\n");
     return 1;
   }
@@ -31,33 +33,33 @@ int init_debugging() {
 #define IDebug(arg) printf("arg = %d\n", arg);
 
 #ifdef SHOULD_DEBUG_BEHAVIOR
-#define BehaviorLog(...) printf(__VA_ARGS__); printf("\n"); fprintf(behavior, __VA_ARGS__); fprintf(behavior, "\n")
+#define BehaviorLog(...) printf(__VA_ARGS__); printf("\n"); fprintf(logbehavior, __VA_ARGS__); fprintf(logbehavior, "\n")
 #define BehaviorIDebug(arg) printf("arg = %d\n", arg);
 #else
-#define BehaviorLog(...) fprintf(behavior, __VA_ARGS__); fprintf(behavior, "\n")
+#define BehaviorLog(...) fprintf(logbehavior, __VA_ARGS__); fprintf(logbehavior, "\n")
 #define BehaviorIDebug(arg)
 #endif
 
 #ifdef SHOULD_DEBUG_MOVEMENT
-#define MovementLog(...) printf(__VA_ARGS__); printf("\n"); fprintf(movement, __VA_ARGS__); fprintf(movement, "\n")
+#define MovementLog(...) printf(__VA_ARGS__); printf("\n"); fprintf(logmovement, __VA_ARGS__); fprintf(logmovement, "\n")
 #define MovementIDebug(arg) printf("arg = %d\n", arg)
 #else
-#define MovementLog(...) fprintf(movement, __VA_ARGS__); fprintf(movement, "\n")
+#define MovementLog(...) fprintf(logmovement, __VA_ARGS__); fprintf(logmovement, "\n")
 #define MovementIDebug(arg)
 #endif
 
 #ifdef SHOULD_DEBUG_SETUP
-#define SetupLog(...) printf(__VA_ARGS__); printf("\n"); fprintf(setup, __VA_ARGS__); fprintf(setup, "\n")
+#define SetupLog(...) printf(__VA_ARGS__); printf("\n"); fprintf(logsetup, __VA_ARGS__); fprintf(logsetup, "\n")
 #define SetupIDebug(arg) printf("arg = %d\n", arg);
 #else
-#define SetupLog(...) fprintf(setup, __VA_ARGS__); fprintf(setup, "\n")
+#define SetupLog(...) fprintf(logsetup, __VA_ARGS__); fprintf(logsetup, "\n")
 #define SetupIDebug(arg)
 #endif
 
 #ifndef SHOULD_DEBUG_SENSOR
-#define SensorLog(...) fprintf(sensor, __VA_ARGS__); fprintf(sensor, "\n")
+#define SensorLog(...) fprintf(logsensor, __VA_ARGS__); fprintf(logsensor, "\n")
 #define SensorIDebug(arg)
 #else
-#define SensorLog(...) printf(__VA_ARGS__); printf("\n"); fprintf(sensor, __VA_ARGS__); fprintf(sensor, "\n")
+#define SensorLog(...) printf(__VA_ARGS__); printf("\n"); fprintf(logsensor, __VA_ARGS__); fprintf(logsensor, "\n")
 #define SensorIDebug(arg) printf("arg = %d\n", arg);
 #endif
